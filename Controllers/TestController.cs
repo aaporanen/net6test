88f3a4324313bdf5914488f06524a53e4248fa58
Aapeli Poranen <aapeli.poranen@gmail.com>
2022-04-16 14:42:33 +0300

    remove weather forecast stuff.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace net6test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { hello = "world" });
+    }
+}
