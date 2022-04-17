28eb180d4ea8636e2991852e4aa76f5971013d32
Aapeli Poranen <aapeli.poranen@gmail.com>
2022-04-17 12:44:05 +0300

    cosmosdbtest

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Azure.Cosmos;
 namespace net6test.Controllers;
 
 [ApiController]
@@ -7,15 +7,29 @@ namespace net6test.Controllers;
 public class TestController : ControllerBase
 {
     private readonly ILogger<TestController> _logger;
+    private static readonly string _endpoint = "https://cosmostestnet6.documents.azure.com:443/";
 
-    public TestController(ILogger<TestController> logger)
+    private readonly IConfiguration _conf;
+
+    public TestController(ILogger<TestController> logger, IConfiguration conf)
     {
         _logger = logger;
+        _conf = conf;
     }
 
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(new { hello = "world" });
+        var dbclient = new CosmosClient(_endpoint, _conf.GetValue<string>("CosmosDBKey"));
+        var db = dbclient.GetDatabase("test");
+        var container = db.GetContainer("test");
+        var items = container.GetItemLinqQueryable<Test>(true).ToList();
+        return Ok(items);
+    }
+
+    public class Test
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
